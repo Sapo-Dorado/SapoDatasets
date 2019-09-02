37fed1dffa1e968807e67901f71a818037bb13e0
Nicholas Brown <countdooku.steak@gmail.com>
2019-09-01 19:05:40 -0700

    fixed a type bug

--- code/Utils.jl
+++ code/Utils.jl
@@ -1,4 +1,4 @@
 #changes the size of the images accesed by the list
-transform(list::Union{ItemList,SplitItemList,LabeledItemList}, newSize::Int) = list.size = newSize 
+transform(list, newSize::Int) = list.size = newSize 
 
 
